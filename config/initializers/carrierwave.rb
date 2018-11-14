0d45f82c8cc3eeed8dca1980cbbeaf2c7d9d21c0
EC2 Default User <ec2-user@ip-172-31-11-10.us-east-2.compute.internal>
2018-11-14 02:21:49 +0000

    first commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-#     aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-#     region: 'us-east-2'
-#   }
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region: 'us-east-2'
+  }
  
-# end
+    config.fog_directory  = 'rails-photo-123'
+    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/create-box"
+    config.cache_storage = :fog
+    
+end
