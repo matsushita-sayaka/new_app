06730d6164e4b2979f038a58a5e4e8349366faa5
EC2 Default User <ec2-user@ip-172-31-11-10.us-east-2.compute.internal>
2018-11-19 14:19:35 +0000

    “new_app”

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,10 @@ class RegistrationsController < Devise::RegistrationsController
     def after_sign_up_path_for(resource)
         user_path(resource)
     end
+    
+    def after_sign_in_path_for(resource)
+        user_path(resource)
+    end
 
     private
     
