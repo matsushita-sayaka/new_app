07fe45ec71374cb1441a068161e13fdfb8264aa4
EC2 Default User <ec2-user@ip-172-31-11-10.us-east-2.compute.internal>
2018-11-19 23:37:03 +0000

    “new_app”

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_user, only: :destroy
-  before_action :friend_user, only: :talk
   
   def index
     @users = User.paginate(page: params[:page], per_page: 15)
@@ -91,13 +90,6 @@ class UsersController < ApplicationController
   
   def admin_user
     redirect_to(root_path) unless current_user.admin?
-  end
-  
-  def friend_user
-    @user = User.find(params[:id])
-    unless current_user || @user
-      redirect_to(root_path)
-    end
   end
   
 end
